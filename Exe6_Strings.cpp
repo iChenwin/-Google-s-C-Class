378234c63d6f626f67a213976861dcf32d829eff
iChenwin <ichenwin@gmail.com>
2017-09-11 14:18:35 +0800

    comment

--- Exe6_Strings.cpp
+++ Exe6_Strings.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
   string str1 = "To be or not to be, that is the question";
   string str2 = "only ";
-  string str3 = str1.substr(6, 12);
-  str1.insert(32, str2);
-  str1.replace(str1.find("to be", 0), 5, "to jump");
-  str1.erase(9, 4);
+  string str3 = str1.substr(6, 12);   //pos, length. "or not to be"
+  str1.insert(32, str2);    //pos, string. ""To be or not to be, that is the only question"
+  str1.replace(str1.find("to be", 0), 5, "to jump");    //replace(pos, length, string)  "To be or not to jump, that is the only question"
+  str1.erase(9, 4);     //(pos, length)  "To be or to jump, that is the only question"
   cout << str1 << endl;
   for (int i = 0; i < str3.length(); i++)
     cout << str3[i]; cout << endl;
