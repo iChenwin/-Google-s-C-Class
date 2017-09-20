f36a5dffa4d051152ac8c20c966d57eab0721b9b
iChenwin <ichenwin@gmail.com>
2017-09-20 22:02:35 +0800

    time class

---
+++ examples/nsExamp4_timeClass.cpp
@@ -0,0 +1,66 @@
+// time.cpp, Maggie Johnson
+// Description: A simple time class.
+
+#include <iostream>
+using namespace std;
+
+class Time {
+ private:
+  int hours_;
+  int minutes_;
+  int seconds_;
+ public:
+  void set(int h, int m, int s) {hours_ = h; minutes_ = m; seconds_ = s; return;}
+  void increment();
+  void decrement();
+  void display();
+};
+
+void Time::increment() {
+  seconds_++;
+  minutes_ += seconds_/60;
+  hours_ += minutes_/60;
+  seconds_ %= 60;
+  minutes_ %= 60;
+  hours_ %= 24;
+  return;
+}
+
+void Time::decrement() {
+    if (0 == seconds_) {
+        seconds_ = 59;
+        if (0 == minutes_) {
+            minutes_ = 59;
+            if (0 == hours_) {
+                hours_ = 23;
+            } else {
+                hours_--;
+            }
+        } else {
+            minutes_--;
+        }
+    } else {
+        seconds_--;
+    }
+}
+
+void Time::display() {
+  cout << (hours_ % 12 ? hours_ % 12:12) << ':'
+       << (minutes_ < 10 ? "0" :"") << minutes_ << ':'
+       << (seconds_ < 10 ? "0" :"") << seconds_
+       << (hours_ < 12 ? " AM" : " PM") << endl;
+}
+
+int main() {
+  Time timer;
+//   timer.set(23,59,58);
+    timer.set(12,0,0);
+  for (int i = 0; i < 5; i++) {
+    // timer.increment();
+    timer.display();
+    timer.decrement();
+    cout << endl;
+
+
+  }
+}
